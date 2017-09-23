348f0adcf8cf574b928be4219058a4c28a2e3e0f
Nick <nickui@users.noreply.github.com>
2017-09-22 23:57:56 -0700

    Update connection.js
    added a port for mysql

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    //port: 3306,
+    port: 3306, //not sure if this needs to be set
     host: "us-cdbr-iron-east-05.cleardb.net",
     user: "ba0cf03fcf7397",
     password: "6c19e758",
